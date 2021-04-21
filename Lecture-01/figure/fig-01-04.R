0954bd654cff07caf9d645c998b3d9e8bdc0faaa
NAKAJIMA, Yukihiro <yukihiro.618.soa@gmail.com>
2021-04-21 14:33:05 +0900

    initial commit

---
+++ Lecture-01/figure/fig-01-04.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(dplyr)
+data.frame(
+  x = c(0, 4),
+  y = c(1, 3)
+) %>% 
+  ggplot(aes(x, y)) +
+  geom_line() +
+  scale_y_continuous(breaks=seq(0, 3,by = 1),limits=c(0,3)) +
+  theme_bw(base_size = 20)
